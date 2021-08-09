18fa310801a606feb7eb891148132763f32a3102
Max Herkt <herkt@wave-solutions.de>
2021-08-09 18:31:37 +0200

    Added CookieService for writing tokens to cookies

---
+++ Infrastructure.IdentityLibrary/Services/CookieService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Infrastructure.IdentityLibrary.Services
+{
+    internal class CookieService : ICookieService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private IResponseCookies ResponseCookies => _httpContextAccessor.HttpContext.Response.Cookies;
+        private IRequestCookieCollection RequestCookies => _httpContextAccessor.HttpContext.Request.Cookies;
+
+        public CookieService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public string Get(string key) => RequestCookies[key];
+
+        public void Set(string key, string value, int expireInHours)
+        {
+            var option = new CookieOptions
+            {
+                Expires = DateTime.Now.AddMinutes(expireInHours)
+            };
+
+            ResponseCookies.Append(key, value, option);
+        }
+
+        public void Remove(string key) => ResponseCookies.Delete(key);
+    }
+}
---
+++ Infrastructure.IdentityLibrary/Services/ICookieService.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.IdentityLibrary.Services
+{
+    internal interface ICookieService
+    {
+        string Get(string key);
+        void Remove(string key);
+        void Set(string key, string value, int expireInHours);
+    }
+}
